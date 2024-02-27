361de92901a2e3c50c29a234d9f51f6ea97fce37
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-02-26 17:40:35 -0800

    Initial Project Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-app';
+  
+  @ViewChild('slider') slider!: ElementRef; // Note the '!' to indicate that it will be initialized later
+  slideWidth!: number; // Note the '!' to indicate that it will be initialized later
+  scrollPosition: number = 0;
+
+  ngAfterViewInit() {
+    this.slideWidth = this.slider.nativeElement.offsetWidth;
+  }
+
+  prev() {
+    if (this.scrollPosition > 0) {
+      this.scrollPosition -= this.slideWidth;
+      this.slider.nativeElement.scrollTo({
+        left: this.scrollPosition,
+        behavior: 'smooth'
+      });
+    }
+  }
+
+  next() {
+    if (this.scrollPosition < this.slider.nativeElement.scrollWidth - this.slider.nativeElement.clientWidth) {
+      this.scrollPosition += this.slideWidth;
+      this.slider.nativeElement.scrollTo({
+        left: this.scrollPosition,
+        behavior: 'smooth'
+      });
+    }
+  }
 }
