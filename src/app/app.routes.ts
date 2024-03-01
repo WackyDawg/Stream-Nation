85633afac0dcd4a216c79db9f80356b347632e22
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-03-01 22:49:24 +0100

    Added Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LiveTvComponent } from './components/live-tv/live-tv.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'en/live-tv', component: LiveTvComponent },
+    { path: 'en/search', component: SearchComponent }
+];
+
+
+@NgModule ({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
