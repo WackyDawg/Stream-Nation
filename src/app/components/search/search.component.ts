6b4298e7f9a89e5c744c8573398bbbc86712042f
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-03-01 22:54:59 +0100

    Created New Component

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { VideoComponent } from '../video/video.component';
+import { ChannelMetadataComponent } from '../channel-metadata/channel-metadata.component';
+import { OverlayPlayerComponent } from '../overlay-player/overlay-player.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { MobileNavComponent } from '../mobile-nav/mobile-nav.component';
+import { LoginDialogComponent } from '../login-dialog/login-dialog.component';
+import { SearchMainComponent } from '../pages/search-main/search-main.component';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    VideoComponent,
+    ChannelMetadataComponent,
+    OverlayPlayerComponent,
+    SidebarComponent,
+    MobileNavComponent,
+    LoginDialogComponent,
+    SearchMainComponent,
+  ],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css',
+})
+export class SearchComponent {}
