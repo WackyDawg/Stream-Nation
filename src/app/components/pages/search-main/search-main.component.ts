5973244fa1051df26da32162aba800fb19f966ee
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-03-01 22:54:26 +0100

    Created New Component

---
+++ src/app/components/pages/search-main/search-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-main.component.html',
+  styleUrl: './search-main.component.css'
+})
+export class SearchMainComponent {
+
+}
