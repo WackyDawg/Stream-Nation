8ed546e6abc1511d2551d4c814221bccc6f17b5a
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-03-01 22:50:25 +0100

    Added RouterLink Module

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router'
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { CommonModule } from '@angular/common';;
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private router: Router) { }
 
+  isCurrentPage(path: string): boolean {
+    return this.router.url === path;
+  }
+
+  navigateAndReload(path: string): void {
+    this.router.navigateByUrl(path).then(() => {
+      window.location.reload();
+    });
+  }
+ 
   menuVisible: boolean = false;
 
   toggleMenu() {
@@ -18,5 +33,6 @@ export class HeaderComponent {
   closeMenu() {
     this.menuVisible = false;
   }
+  
 
 }
