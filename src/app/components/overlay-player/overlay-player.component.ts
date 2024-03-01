be53326b04ea1477d8eec994f5ac4ce4bdc3ecfc
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-02-29 21:55:50 -0800

    Created New Component

---
+++ src/app/components/overlay-player/overlay-player.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overlay-player',
+  standalone: true,
+  imports: [],
+  templateUrl: './overlay-player.component.html',
+  styleUrl: './overlay-player.component.css'
+})
+export class OverlayPlayerComponent {
+
+}
