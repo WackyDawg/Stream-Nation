fb5d64f0813019ea3775dda660a7e503b90e4523
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-03-01 22:53:34 +0100

    Created New Component

--- src/app/components/live-tv/live-tv.component.ts
+++ src/app/components/live-tv/live-tv.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { ChannelMetadataComponent } from '../channel-metadata/channel-metadata.component';
+import { MainComponent } from '../main/main.component';
+import { MobileNavComponent } from '../mobile-nav/mobile-nav.component';
+import { OverlayPlayerComponent } from '../overlay-player/overlay-player.component';
+import { VideoComponent } from '../video/video.component';
 
 @Component({
   selector: 'app-live-tv',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, HeaderComponent, ChannelMetadataComponent, MainComponent, MobileNavComponent, OverlayPlayerComponent,VideoComponent],
   templateUrl: './live-tv.component.html',
   styleUrl: './live-tv.component.css'
 })
