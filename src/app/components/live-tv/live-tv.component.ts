cd8208a0c984b21d0f594a51532c9e15e5b00387
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-03-01 15:56:23 +0100

    Created New Component

---
+++ src/app/components/live-tv/live-tv.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-live-tv',
+  standalone: true,
+  imports: [],
+  templateUrl: './live-tv.component.html',
+  styleUrl: './live-tv.component.css'
+})
+export class LiveTvComponent {
+
+}
