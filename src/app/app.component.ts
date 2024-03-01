5aca18a33ea0ad332d1d55e4ab215ae1e0d8d7be
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-03-01 22:48:59 +0100

    Created New Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,13 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { VideoComponent } from './components/video/video.component';
-import { MainComponent } from './components/main/main.component';
-import { MobileNavComponent } from './components/mobile-nav/mobile-nav.component';
-import { ChannelMetadataComponent } from './components/channel-metadata/channel-metadata.component';
-import { OverlayPlayerComponent } from './components/overlay-player/overlay-player.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { LiveTvComponent } from './components/live-tv/live-tv.component';
+import { SearchComponent } from './components/search/search.component';
 // import { VideoContainerComponent } from './components/video-container/video-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,VideoComponent,MainComponent,MobileNavComponent,ChannelMetadataComponent,OverlayPlayerComponent,LiveTvComponent],
+  imports: [RouterOutlet, RouterLink, LiveTvComponent,SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
