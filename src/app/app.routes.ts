ba749fa99ccdf08f1feb0bbc46d14d7976140653
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-03-04 18:39:28 +0100

    Organised folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LiveTvComponent } from './components/live-tv/live-tv.component';
-import { HomeComponent } from './components/home/home.component';
-import { SearchComponent } from './components/search/search.component';
+import { LiveTvComponent } from './pages/live-tv/live-tv.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { OnDemandComponent } from './pages/on-demand/on-demand.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'en/live-tv', component: LiveTvComponent },
-    { path: 'en/search', component: SearchComponent }
+    { path: 'en/search', component: SearchComponent },
+    { path: 'en/on-demand', component: OnDemandComponent },
 ];
 
 
